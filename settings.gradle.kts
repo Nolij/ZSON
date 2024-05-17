bcdc4e372c6daf6fa950a74ca27a715e7406492d
Nolij <xdmatthewbx@mixhosting.xyz>
2024-05-17 18:02:16 -0400

    add CI, auto-versioning, sources jar, and Jenkins support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,17 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+pluginManagement {
+    repositories {
+        gradlePluginPortal {
+            content {
+                excludeGroup("org.apache.logging.log4j")
+            }
+        }
+    }
 }
-rootProject.name = "ZSON"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
+    id("org.ajoberstar.grgit") version("5.2.2") apply(false)
+}
+
+rootProject.name = "zson"
 
