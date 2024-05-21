c71946fa7dc865b60943c646523218dc562dbd6d
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-05-20 22:02:09 -0400

    make java 8 compatible actually

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
     id("org.ajoberstar.grgit") version("5.2.2") apply(false)
     id("com.github.breadmoirai.github-release") version("2.4.1") apply(false)
-    id("xyz.wagyourtail.jvmdowngrader") version("0.3.0") apply(false)
+    id("xyz.wagyourtail.jvmdowngrader") version("0.4.1-SNAPSHOT") apply(false)
 }
 
 rootProject.name = "zson"
